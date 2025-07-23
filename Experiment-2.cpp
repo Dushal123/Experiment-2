818ba45dc5916546c63e37f9c910d36e6d5c6212
Dushal123 <kelladushal501@gmail.com>
2025-07-23 21:15:40 +0530

    Add files via upload

---
+++ Experiment-2.cpp
@@ -0,0 +1,26 @@
+/*
+Name:Dushal kella
+prn:24070123053
+Batch:ENTC A2
+*/
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 20;
+    float height = 5.9f;
+    double weight = 65.75;
+    char grade = 'A';
+    bool isStudent = true;
+
+    cout << "Age: " << age << endl;
+    cout << "Height: " << height << " feet" << endl;
+    cout << "Weight: " << weight << " kg" << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Is Student: " << isStudent << endl;
+
+    return 0;
+}
